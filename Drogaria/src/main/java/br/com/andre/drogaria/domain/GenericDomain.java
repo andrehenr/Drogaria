2a91b964d50367c409cf6ba29aa00294c4705326
XI324337 <p000lgouveia@prservicos.com.br>
2018-07-17 17:15:51 -0300

    Primeiro Commit

--- Drogaria/src/main/java/br/com/andre/drogaria/domain/GenericDomain.java
+++ Drogaria/src/main/java/br/com/andre/drogaria/domain/GenericDomain.java
@@ -2,10 +2,12 @@ package br.com.andre.drogaria.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public class GenericDomain implements Serializable{
+	@Id
 	private Long codigo;
 	
 	public Long getCodigo() {
